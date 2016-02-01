3b341fd0709ad9b7d809608d120aeb5b27402a45
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2016-02-01 16:18:21 +0700

    10/10 with background

--- src/diagram/element/Rectangle.java
+++ src/diagram/element/Rectangle.java
@@ -1,5 +1,6 @@
 package diagram.element;
 
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Point;
 
@@ -12,13 +13,23 @@ public class Rectangle extends AElement {
 	@Override
 	public void draw() {
 		GC gc = new GC(getCanvas());
+		Color black = new Color(gc.getDevice(), 0, 0, 0);
+		Color white = new Color(gc.getDevice(), 255, 255, 255);
+		gc.setForeground(black);
+		gc.setBackground(white);
 		gc.drawRectangle(getX(), getY(), getWidth(), getHeight());
+		gc.fillRectangle(getX() + 1, getY() + 1, getWidth() - 1, getHeight() - 1);
 	}
 
 	@Override
 	public boolean checkBoundary(int x, int y) {
-		// TODO Auto-generated method stub
-		return false;
+		if (x < super.x || x > super.x + super.width) {
+			return false;
+		}
+		if (y < super.y || y > super.y + super.height) {
+			return false;
+		}
+		return true;
 	}
 
 }
