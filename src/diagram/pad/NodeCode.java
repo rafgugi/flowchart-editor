27db9a65754ac923b7a3234caa1fda6bdec08186
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2016-03-14 15:40:24 +0700

    little change

--- src/diagram/pad/NodeCode.java
+++ src/diagram/pad/NodeCode.java
@@ -1,44 +1,44 @@
 package diagram.pad;
 
 public class NodeCode {
-	
+
 	public NodeCode parent;
-	public int x;
-	public int y;
-	private int xStreak;
-	private int yStreak;
-	
+	public int x; // current x value
+	public int y; // current y value
+	private int xStreak; // child highest x value
+	private int yStreak; // child highest y value
+
 	public NodeCode(NodeCode parent, int x, int y) {
 		this.parent = parent;
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public NodeCode(NodeCode parent) {
 		this(parent, 0, parent.getYStreak());
-		parent.incYStreak();
+		incYStreak();
 	}
-	
+
 	public NodeCode() {
 		parent = null;
 		xStreak = 0;
 		yStreak = 0;
 	}
-	
+
 	public int getXStreak() {
 		return xStreak;
 	}
-	
+
 	public void incXStreak() {
 		xStreak++;
 	}
-	
+
 	public int getYStreak() {
 		return yStreak;
 	}
-	
+
 	public void incYStreak() {
 		yStreak++;
 	}
-	
+
 }
