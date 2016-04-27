9b9c8b94e95414ea7d3470be579c282d241d3bd8
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2016-04-27 10:35:00 +0700

    convert tab to spaces, for easier reading on github because this is only pseudocode

--- Pseudo_CodeAlgorithm.java
+++ Pseudo_CodeAlgorithm.java
@@ -9,89 +9,89 @@ Let the code of node be a string;
 CodeAlgorithm(root, root.son, 0000) /*start up recursion*/ [0]
 CodeAlgorithm(Node Father, Node CurrentNode, String CurrentCode)
 {
-	if(CurrentNode is Process) [1]
-	{
-		CurrentNode.code= CurrentCode /*Generate the code for CurrentNode;*/ [1-1]
-		CodeofSon = Increase YY of CurrentCode by one
-		if(CurrentNode.hasNotBeenTraversed())
-		{
-			CodeAlgorithm(CurrentNode, CurrentNode.Son, CodeofSon); [2]
-		}
-		else
-		{
-			/* Father is recognized as a do-while structure, mark the Judgment and
-			link it with his Father, include do-while and nested do-while */
-			if(Father is Judgment and Father.hasNotBeenRecognized()) [3]
-			{
-				Father.type = 'do-while';
-				CurrentNode.doWhileCounter++; /*original value is zero*/
-				Father.doWhileNode=CurrentNode;
-				Father. doWhileCounter= CurrentNode.doWhileCounter;
-				CurrentNode.doWhileRecodeCounter=CurrentNode.doWhileCounter;/*used for recode*/
-			}
-			if(CurrentNode.doWhileRecodeCounter>0) Recode(CurrentNode); [4]
-		}
-	}
-	if(CurrentNode is Judgment) [5]
-	{
+    if(CurrentNode is Process) [1]
+    {
+        CurrentNode.code= CurrentCode /*Generate the code for CurrentNode;*/ [1-1]
+        CodeofSon = Increase YY of CurrentCode by one
+        if(CurrentNode.hasNotBeenTraversed())
+        {
+            CodeAlgorithm(CurrentNode, CurrentNode.Son, CodeofSon); [2]
+        }
+        else
+        {
+            /* Father is recognized as a do-while structure, mark the Judgment and
+            link it with his Father, include do-while and nested do-while */
+            if(Father is Judgment and Father.hasNotBeenRecognized()) [3]
+            {
+                Father.type = 'do-while';
+                CurrentNode.doWhileCounter++; /*original value is zero*/
+                Father.doWhileNode=CurrentNode;
+                Father. doWhileCounter= CurrentNode.doWhileCounter;
+                CurrentNode.doWhileRecodeCounter=CurrentNode.doWhileCounter;/*used for recode*/
+            }
+            if(CurrentNode.doWhileRecodeCounter>0) Recode(CurrentNode); [4]
+        }
+    }
+    if(CurrentNode is Judgment) [5]
+    {
 
-		if(CurrentNode.hasNotBeenTraversed()) /*first in*/ [5-2]
-		{
-			CurrentNode.code= CurrentCode /*Generate the code for CurrentNode;*/ [5-1]
-			Stack.push(StackofJudgement, Judgment) /*push Judgment into StackofJudgement */ [6]
+        if(CurrentNode.hasNotBeenTraversed()) /*first in*/ [5-2]
+        {
+            CurrentNode.code= CurrentCode /*Generate the code for CurrentNode;*/ [5-1]
+            Stack.push(StackofJudgement, Judgment) /*push Judgment into StackofJudgement */ [6]
 
-			for every son j of CurrentNode (except for convergence) do /* j starts from 0 */ [7]
-			{
-				CurrentCode = CurrentCode+0j00； /* nex layer */
-				CodeAlgorithm(CurrentNode, CurrentNode.Son, CurrentCode); [7-1]
-			}
-			if CurrentNode has a son of convergence( as convergenceSon ) [7-2]
-			{
-				CodeAlgorithm(CurrentNode, convergenceSon, null); [8]
-			}
-			if(CurrentNode.isNotRecognized()) /* loop structures have been recognized, the left is selections */
-			{
-				/* according to the condition of judgment, the detailed 
-				structures of if-else/if/case can be recognized also.*/
-				CurrentNode.type = 'selection'; /* Recognized as selection structures; */
-			}
-			/*Continue to process the nodes behind Convergence. */
-			CurrentNode=CurrentNode.directJudgmentNode;
-			CodeofSon = Increase YY of CurrentCode by one
-			CodeAlgorithm(CurrentNode, CurrentNode.Son, CodeofSon); [9]
-		}
-		else /* been traversed */
-		{
-			if(CurrentNode.isNotRecognized()) /* the first round trip */ [10]
-			{
-				CurrentNode.type = 'while'; /* recognized as while or for structures; */
-			}
-			else /* been traversed and recognized */
-			{
-				if(Father is Judgment and Father.hasNotBeenRecognized()) [11]
-				{
-					Father.type = 'do-while';
-					CurrentNode.doWhileCounter++; /* original value is zero */
-					Father.doWhileNode=CurrentNode;
-					Father. doWhileCounter= CurrentNode.doWhileCounter;
-					/*used for recode*/
-					CurrentNode.doWhileRecodeCounter=CurrentNode.doWhileCounter;
-				}
-				if(CurrentNode.doWhileRecodeCounter>0) Recode(CurrentNode,null); [12]
-			}
-		}
-	}
-	if(CurrentNode is Convergence)
-	{
-		if(CurrentNode.hasNotBeenTraversed()) /* match a judgment node and a convergence node */ [13]
-		{
-			/* as a judgment and a convergence is exist geminate, so the top
-			judgment in stack must be able to match the current convergence */
-			tempJudgeNode=Stack.Pop(StackofJudgement);
-			CurrentNode.directJudgmentNode= tempJudgeNode;
-			tempJudgeNode.directJudgmentConvergence= CurrentNode;
-			CurrentNode.code= tempJudgeNode.code;
-		} else return; [14]
-	}
-	if(CurrentNode is End) return; [15]
+            for every son j of CurrentNode (except for convergence) do /* j starts from 0 */ [7]
+            {
+                CurrentCode = CurrentCode+0j00； /* nex layer */
+                CodeAlgorithm(CurrentNode, CurrentNode.Son, CurrentCode); [7-1]
+            }
+            if CurrentNode has a son of convergence( as convergenceSon ) [7-2]
+            {
+                CodeAlgorithm(CurrentNode, convergenceSon, null); [8]
+            }
+            if(CurrentNode.isNotRecognized()) /* loop structures have been recognized, the left is selections */
+            {
+                /* according to the condition of judgment, the detailed 
+                structures of if-else/if/case can be recognized also.*/
+                CurrentNode.type = 'selection'; /* Recognized as selection structures; */
+            }
+            /*Continue to process the nodes behind Convergence. */
+            CurrentNode=CurrentNode.directJudgmentNode;
+            CodeofSon = Increase YY of CurrentCode by one
+            CodeAlgorithm(CurrentNode, CurrentNode.Son, CodeofSon); [9]
+        }
+        else /* been traversed */
+        {
+            if(CurrentNode.isNotRecognized()) /* the first round trip */ [10]
+            {
+                CurrentNode.type = 'while'; /* recognized as while or for structures; */
+            }
+            else /* been traversed and recognized */
+            {
+                if(Father is Judgment and Father.hasNotBeenRecognized()) [11]
+                {
+                    Father.type = 'do-while';
+                    CurrentNode.doWhileCounter++; /* original value is zero */
+                    Father.doWhileNode=CurrentNode;
+                    Father. doWhileCounter= CurrentNode.doWhileCounter;
+                    /*used for recode*/
+                    CurrentNode.doWhileRecodeCounter=CurrentNode.doWhileCounter;
+                }
+                if(CurrentNode.doWhileRecodeCounter>0) Recode(CurrentNode,null); [12]
+            }
+        }
+    }
+    if(CurrentNode is Convergence)
+    {
+        if(CurrentNode.hasNotBeenTraversed()) /* match a judgment node and a convergence node */ [13]
+        {
+            /* as a judgment and a convergence is exist geminate, so the top
+            judgment in stack must be able to match the current convergence */
+            tempJudgeNode=Stack.Pop(StackofJudgement);
+            CurrentNode.directJudgmentNode= tempJudgeNode;
+            tempJudgeNode.directJudgmentConvergence= CurrentNode;
+            CurrentNode.code= tempJudgeNode.code;
+        } else return; [14]
+    }
+    if(CurrentNode is End) return; [15]
 }
--- Pseudo_CodeGenerate.java
+++ Pseudo_CodeGenerate.java
@@ -8,32 +8,32 @@ CodeGenerate(beginBlock, TempCode);
 PrintAndFormatCode(TempCode); /*output and format the code*/
 CodeGenerate(Block CurrentBlock, String CurrentCode)
 {
-	if(CurrentBlock.NodeType is Process) CurrentCode.Append(CurrentBlock.codeBlock);
-	else if(CurrentBlock.NodeType is Selection)
-	{
+    if(CurrentBlock.NodeType is Process) CurrentCode.Append(CurrentBlock.codeBlock);
+    else if(CurrentBlock.NodeType is Selection)
+    {
 
-		Generate branch code as SelectionCode;
-		for every SubNode in CurrentBlock.SubNodeList do
-		{
-			Generate branch code branchCode;
-			String branchBody;
-			CodeGenerate(SubNode, branchBody);
-			Insert branchBody into branchCode;
-			SelectionCode.Append(branchCode);
-		}
-		CurrentBlock.Append(SelectionCode);
-	}
-	else if(CurrentBlock.NodeType is HeadPtr) /*If type is HeadPtr, then process the sub-nodes*/
-	{
-		for every SubNode in CurrentBlock.SubNodeList do CodeGenerate(SubNode, CurrentCode);
-	}
-	else if(CurrentBlock.NodeType is Loop)
-	{
-		Generate loop code as loopCode;
-		String loopBody;
-		for every SubNode in CurrentBlock.SubNodeList do CodeGenerate(SubNode, loopBody);
-			insert loopBody into loopCode;
-		CurrentBlock.Append(loopCode);
-	}
-	else return;
+        Generate branch code as SelectionCode;
+        for every SubNode in CurrentBlock.SubNodeList do
+        {
+            Generate branch code branchCode;
+            String branchBody;
+            CodeGenerate(SubNode, branchBody);
+            Insert branchBody into branchCode;
+            SelectionCode.Append(branchCode);
+        }
+        CurrentBlock.Append(SelectionCode);
+    }
+    else if(CurrentBlock.NodeType is HeadPtr) /*If type is HeadPtr, then process the sub-nodes*/
+    {
+        for every SubNode in CurrentBlock.SubNodeList do CodeGenerate(SubNode, CurrentCode);
+    }
+    else if(CurrentBlock.NodeType is Loop)
+    {
+        Generate loop code as loopCode;
+        String loopBody;
+        for every SubNode in CurrentBlock.SubNodeList do CodeGenerate(SubNode, loopBody);
+            insert loopBody into loopCode;
+        CurrentBlock.Append(loopCode);
+    }
+    else return;
 }
