141c25ac82993708742f7ce627d9013c497961b8
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2016-04-28 01:24:44 +0700

    new type, to accommodate generation flowchart element code

--- Pseudo_Recode.java
+++ Pseudo_Recode.java
@@ -28,28 +28,28 @@ Recode(Node CurrentNode, String CurrentCode)
         CurrentNode.doWhileRecodeCounter--;
         Recode(tfather, CurrentNode.Code); /*Let the code of tfather be the code of CurrentNode*/ [R6]
         if(CurrentNode.doWhileRecodeCounter==0) [R7]
-            CurrentNode.doWhileRecodeCounter= CurrentNode.doWhileCounter; 
+            CurrentNode.doWhileRecodeCounter= CurrentNode.doWhileCounter;
     }
     if(CurrentNode.type is Process) [R8]
     {
-        CurrentCode←CurrentCode.YY+1; /*the sequence part of code increase one*/
+        CurrentCode = CurrentCode.YY+1; /*the sequence part of code increase one*/
         Recode(CurrentNode.son, CurrentCode,); [R8-1]
     }
     else if(CurrentNode.type is selection)
     {
         for every son j of CurrentNode (except for convergence) do /*j starts from 0*/ [R9]
         {
-            CurrentCode =currentCode+0j00； /*nex layer*/
+            CurrentCode = currentCode+0j00； /*nex layer*/
             Recode(son, CurrentCode);
         }
         Recode(CurrentNode.directJudgmentConvergence.son, CurrentCode); [R10]
     }
     else if(CurrentNode.type is loop) [R11]
     {
-        CurrentCode =currentCode+0000; /*nex layer*/
+        CurrentCode = currentCode+0000; /*nex layer*/
         Recode(CurrentNode.son, CurrentCode); /* CurrentNode.son is not the convergence */ [R11-1]
         if(LoopReturnStack.top==null) return; /*return to CodeAlgorithm*/ [R13]
-        CurrentCode←CurrentCode.YY+1;
+        CurrentCode = CurrentCode.YY+1;
         Recode(CurrentNode.directJudgmentConvergence.son, CurrentCode); [R11-2]
     }
     else return; [R12]
