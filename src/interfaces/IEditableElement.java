ae15483abcd2298d00480e5c587b541e9912dbcf
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2016-05-07 22:53:12 +0700

    add some documentations

--- src/interfaces/IEditableElement.java
+++ src/interfaces/IEditableElement.java
@@ -9,12 +9,15 @@ public interface IEditableElement extends IElement {
 	public void createEditPoints();
 
 	/**
-	 * Triggered when the element is dragged
-	 *
+	 * Triggered when the element is dragged by another element. Parameter e is
+	 * usually an EditPoint. This element perform drag depends on which
+	 * EditPoint is dragged.
+	 * 
 	 * @param x1
 	 * @param y1
 	 * @param x2
 	 * @param y2
+	 * @param element
 	 */
 	public void drag(int x1, int y1, int x2, int y2, IElement e);
 
