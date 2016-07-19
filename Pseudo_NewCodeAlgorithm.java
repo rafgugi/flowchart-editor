451a71a736cd8bd1a8af9483f99ee8852b9f52b4
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2016-07-19 15:25:49 +0700

    disimpelin lagi

--- Pseudo_NewCodeAlgorithm.java
+++ Pseudo_NewCodeAlgorithm.java
@@ -7,123 +7,84 @@ stackOfJudgment = new Stack<>();
 doWhileStack = new Stack<>();
 codeAlgorithm(father, son, father.getNodeCode().createSibling());
 
-public void codeAlgorithm(FlowChartElement father, FlowChartElement currElem, NodeCode currCode) {
-  if (currElem is Terminator) {
+function codeAlgorithm(FlowChartElement father, FlowChartElement currElem, NodeCode currCode) 
+  if (currElem is Terminator) 
     return; // exit point of recursion
-  }
 
-  if (currElem.getDoWhileNode() != doWhileStack.peek) {
+  if (currElem.getDoWhileNode() != doWhileStack.peek) 
     reset traversed of currElem // masih butuh recode
-  }
-  if (currElem is Process) {
-    if (currElem isntTraversed) {
+
+  if (currElem is Process) 
+    if (currElem isntTraversed) 
       traverse currElem
       set nodeCode of currElem with currCode
       son = currElem.getFlow().getSon();
 
-      /* Generate the code for sibling */
       codeOfSon = currCode.createSibling();
       codeAlgorithm(currElem, son, codeOfSon);
-    } else {
-      /*
-       * Father is recognized as a do-while structure, mark the
-       * Judgment and link it with his Father, include do-while and
-       * nested do-while
-       */
-      if (father is Judgment && father type not defined or DoWhile) {
-        /* Process's father is do-while */
+
+      if (father is Judgment && father type not defined or DoWhile) 
         set type of father with DoWhile
         thisCode = node code of currElem
-        /* do-while take over child code */
         set nodeCode of father with thisCode
-        /* and reset the child of the code */
         thisCode.clearChildren();
 
-        /* Begining to recode do-while child */
-        doWhileStack.push(father); // push doWhile to stack
+        doWhileStack.push(father);
         father.setDoWhileNode(doWhileStack);
         childCode = create child of thisCode;
         codeAlgorithm(father, currElem, childCode);
-        doWhileStack.pop; // pop doWhile after finish recode
-      }
-    }
-  }
-  if (currElem is Judgment) {
-    if (currElem isntTraversed) {
-       /* Judgment hasn't been traversed */
+        doWhileStack.pop;
+
+  if (currElem is Judgment) 
+    if (currElem isntTraversed) 
       traverse currElem
       set nodeCode of currElem with currCode
 
-      if (currElem.getDirectConvergence() == null) {
-        /* Push into stackOfJudgment, to find convergence */
+      if (currElem.getDirectConvergence() == null) 
         stackOfJudgment.push(currElem);
-      }
 
-      /* Get direct convergence and process other children */
       convergenceson = null
-      for each (sons of currElem as son) {
-        if (son is Convergence) {
+      for each (sons of currElem as son) 
+        if (son is Convergence) 
           convergenceSon = son;
           continue;
-        }
-        /* Go to judgment's child that is not convergence */
+
         sonCode = create child of currCode;
         codeAlgorithm(currElem, son, sonCode);
-      }
 
-      if (convergenceson != null) {
-        /* Go to judgment's direct convergence child */
+      if (convergenceson != null) 
         codeAlgorithm(currElem, convergenceson, null);
-      }
 
-      if (currElem.getType == null) {
+      if (currElem.getType == null) 
         set type of father with Selection
-      }
 
-      /* Go to the element behind Convergence */
       convergence = directConvergence of currElem
       sonCode = create sibling from node code of convergence
       sonNode = son of convergence
       codeAlgorithm(convergence, sonNode, sonCode);
-    }
-    else { // been traversed.
-      if (currElem.getType == null) {
+
+    else  // been traversed.
+      if (currElem.getType == null) 
         set type of father with While
-      }
-      else { // and been recognized
-        if (father is Judgment && father.getType == null) {
-          /* Judgment father is do-while */
+      else  // and been recognized
+        if (father is Judgment && father.getType == null) 
           set type of father with DoWhile
           thisCode = node code of currElem
-          /* do-while take over child code */
           set nodeCode of father with thisCode
-          /* and reset the child of the code */
           thisCode.clearChildren();
 
-          /* Begining to recode do-while child */
-          doWhileStack.push(father); // push doWhile to stack
+          doWhileStack.push(father);
           father.setDoWhileNode(doWhileStack);
           childCode = create child of thisCode;
           codeAlgorithm(father, currElem, childCode);
-          doWhileStack.pop; // pop doWhile after finish recode
-        }
-      }
-    }
-  }
-  if (currElem is Convergence) {
-    /* match a judgment node and a convergence node */
-    if (currElem isntTraversed) {
-      /* Convergence hasn't been traversed */
+          doWhileStack.pop;
+
+  if (currElem is Convergence) 
+    if (currElem isntTraversed) 
       traverse currElem
 
-      if (currElem doesnt have directJudgment) {
-        /* Connect convergence with judgment */
+      if (currElem doesnt have directJudgment) 
         judgment = stackOfJudgment.pop;
         set direct judgment of currElem with judgment
         set direct convergence of judgment with currElem
-      }
-      /* Set this convergence node code from direct judgment */
-    }
     set nodeCode of currElem with node code of currElem directJudgment
-  }
-}
--- Pseudo_NewConvertToPAD.java
+++ Pseudo_NewConvertToPAD.java
@@ -2,33 +2,30 @@ firstCode = first code from codeAlgorithm;
 fatherBlock = new BlockContainer;
 convertToPAD(firstCode, fatherBlock);
 
-convertToPAD(currCode, BlockContainer fatherBlock) {
-  if (currCode == null) {
+function convertToPAD(currCode, BlockContainer fatherBlock) 
+  if (currCode == null) 
     return;
-  }
   currElem = currCode.getElement();
-  if (currElem == null) {
+  if (currElem == null) 
     return;
-  }
 
-  if (currElem type is "Process") {
+  if (currElem type is "Process") 
     element = new Sequence;
     element.setText(currElem.getText());
     add element to fatherBlock;
-  }
-  if (currElem type is "Selection") {
+
+  if (currElem type is "Selection") 
     element = new Selection;
     element.setText(currElem.getText());
 
-    foreach currCode children thats not convergence {
+    foreach currCode children thats not convergence 
       nextFlow = child.getElement();
       subBlock = new BlockContainer;
       set subBlock as yes or no child of element;
       convertToPAD(nextFlow.getNodeCode(), subBlock);
-    }
     add element to fatherBlock;
-  }
-  if (currElem type is "DoWhile" OR "While") {
+
+  if (currElem type is "DoWhile" OR "While") 
     element = new While or DoWhile;
     element.setText(currElem.getText());
     subContainer = new BlockContainer;
@@ -37,9 +34,7 @@ convertToPAD(currCode, BlockContainer fatherBlock) {
     set subContainer as element child
     add element to fatherBlock;
     convertToPAD(childCode, subContainer);
-  }
 
   /* Go to next code from the same layer */
   currCode = currCode next sibling;
   convertToPAD(currCode, fatherBlock);
-}
